fa0b9577b9e5d8c773a9b52656eece8ddd890b71
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-19 22:36:53 -0500

    home view styles done

--- woofPalsIonic/www/js/controllers.js
+++ woofPalsIonic/www/js/controllers.js
@@ -1,34 +1,38 @@
 angular.module('app.controllers', [])
-  
+
 .controller('cameraTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('cartTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('cloudTabDefaultPageCtrl', function($scope) {
 
 })
-      
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('userCtrl', function($scope) {
 
 })
-   
-.controller('homeCtrl', function($scope) {
+
+.controller('homeCtrl', function($scope, $ionicPopover) {
+  $ionicPopover.fromTemplateUrl('templates/popover.html', {
+    scope: $scope,
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
 
 })
-   
+
 .controller('sniffsCtrl', function($scope) {
 
 })
-   
+
 .controller('woofsCtrl', function($scope) {
 
 })
- 
