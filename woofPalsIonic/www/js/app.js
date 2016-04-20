bea4c92f17978d40ba24041dc060d2881ab9175e
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-20 15:12:53 -0500

    camera works

--- woofPalsIonic/www/js/app.js
+++ woofPalsIonic/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('woofPals', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+angular.module('woofPals', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
 
 
