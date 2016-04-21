5ab2eece29af0a662d16206561ff76568150c8ed
martinljones <martinleejones@outlook.com>
2016-04-21 10:44:16 -0500

    All routes but connections

--- serverResources/routes/appRoutes.js
+++ serverResources/routes/appRoutes.js
@@ -77,30 +77,57 @@ exports.statusChange = function(req, res){
 };
 
 //Need to make a route that posts to the possibles, connections and rejections reference arrays
-
-exports.postPossibles = function (req, res){
-  // console.log(res);
-  User.findById(req.params.User_id).then(function(user){
-    if(!user)
-      return res.status(404).end;
-
-      //Checking for a match in Connections and Possibles
-      var arr = user.possibles;
-      for(var i = 0; i < arr.length; i++){
-        if(arr[i] === req.body){
-          user.connections.push(req.body);
-          user.save();
-          res.status(200).end();
-        }
-
-        else {
-        user.possibles.push(req.body);
-        user.save();
-        res.status(200).end();
-      }
-    }
-  });
-};
+// 
+// exports.postPossibles = function (req, res){
+//   // console.log(res);
+//   User.findById(req.params.User_id).then(function(user){
+//     if(!user){
+//       console.log(111,"gettingError");
+//       return res.status(404).end();
+//     }
+//     User.findById(req.body._id).then(function(user2) {
+//       if(!user) {
+//         return res.status(404).end();
+//       }
+//
+//       if( user.possibles.length === 0 && user2.possibles.length === 0) {
+//         user.possibles.push(user2._id);
+//         user2.possibles.push(user._id);
+//         user.save();
+//         user2.save();
+//         res.status(200).end();
+//       }
+//       else(user.possibles.length === 0) {
+//
+//       }
+//     });
+//   });
+//       //Checking for a match in Connections and Possibles
+//       // var arr = user.possibles;
+//       // if (arr.length === 0) {
+//       //   if(user.connections.length === 0) {
+//       //     user.possibles.push(req.body);
+//       //     user.save();
+//       //     res.status(200).end();
+//       //   }
+//       // }
+//       // for(var i = 0; i < arr.length; i++){
+//       //   if(arr[i]._id === req.body._id){
+//       //     console.log(req.body);
+//       //     user.connections.push(req.body._id);
+//       //     user.save();
+//       //     res.status(200).end();
+//       //   }
+//       //
+//       //   else {
+//       //   console.log(222, req.body);
+//       //   user.possibles.push(req.body._id);
+//       //   user.save();
+//       //   res.status(200).end();
+//   //     }
+//   //   }
+//   // });
+// };
 
 exports.getConnections = function(req, res){
   User.findById(req.params.User_id).populate(
