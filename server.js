6b64a40e7983604d4df7ac31daf946aaa74c0ba2
martinljones <martinleejones@outlook.com>
2016-04-22 20:18:40 -0500

    added functionality for login with JWT tokens

--- server.js
+++ server.js
@@ -94,6 +94,40 @@
       }
     });
   });
+
+// route to a restricted info (GET http://localhost:8080/api/memberinfo)
+  router.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+      var decoded = jwt.decode(token, config.secret);
+      User.findOne({
+        email: decoded.email
+      }, function(err, user) {
+          if (err) throw err;
+
+          if (!user) {
+            return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+          } else {
+            res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+          }
+      });
+    } else {
+      return res.status(403).send({success: false, msg: 'No token provided.'});
+    }
+  });
+
+  getToken = function (headers) {
+    if (headers && headers.authorization) {
+      var parted = headers.authorization.split(' ');
+      if (parted.length === 2) {
+        return parted[1];
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
 //***********************************************\\
 
 
