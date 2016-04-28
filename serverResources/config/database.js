2c3842148a8c252f2e121f7c9d6770b2088ca6d5
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-04-28 10:21:53 -0500

    whyyyy

--- serverResources/config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'secret':'monkeysridingaunicornwithafish',
-  'database':'mongodb://localhost/pawpals',
-  'mlab': 'mongodb://vanessalvarez8:woofpals3!@ds021701.mlab.com:21701/woofpals'
-};
